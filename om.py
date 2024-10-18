5dc3f9fe1b0c9bbe427c5506e53547d2c4874c42
JJmahen <Mahendra.gc@edu.turkuamk.fi>
2024-10-18 23:25:54 +0300

    files has been renamed

---
+++ om.py
@@ -0,0 +1,13 @@
+# Simple OM symbol using ASCII characters
+
+om_art = '''
+    OOO      M     M
+   O   O     MM   MM
+   O   O     M M M M
+   O   O     M  M  M
+   O   O     M     M
+    OOO      M     M
+'''
+
+print(om_art)
+print("   OM - The Universal Sound")
